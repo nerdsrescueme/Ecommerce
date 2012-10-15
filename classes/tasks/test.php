0069345139924c6409192111333d47aed90978b8
Frank Bardon Jr <frank@nerdsrescue.me>
2012-10-15 15:44:57 -0400

    Ran code cleaner on all code

--- classes/tasks/test.php
+++ classes/tasks/test.php
@@ -4,26 +4,25 @@ namespace Ecommerce\Tasks;
 
 class Test extends \Geek\Design\Task
 {
-	public function run()
-	{
-		$command = \Nerd\VENDOR_PATH . DS . 'bin' . DS . 'phpunit -v -c '
-						 . join(DS, [\Nerd\LIBRARY_PATH, 'ecommerce', 'phpunit.xml']);
+    public function run()
+    {
+        $command = \Nerd\VENDOR_PATH . DS . 'bin' . DS . 'phpunit -v -c '
+                         . join(DS, [\Nerd\LIBRARY_PATH, 'ecommerce', 'phpunit.xml']);
 
-		$this->geek->write('Running tests, please wait for results');
-		$this->geek->write(' ');
+        $this->geek->write('Running tests, please wait for results');
+        $this->geek->write(' ');
 
-		$return = [];
+        $return = [];
 
-		exec($command, $return);
+        exec($command, $return);
 
-		foreach($return as $r)
-		{
-			$this->geek->write($r);
-		}
-	}
+        foreach ($return as $r) {
+            $this->geek->write($r);
+        }
+    }
 
-	public function help()
-	{
-		return 'Write some help!';
-	}
+    public function help()
+    {
+        return 'Write some help!';
+    }
 }
